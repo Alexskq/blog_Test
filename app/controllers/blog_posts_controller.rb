5c28874809723eb4aa8803fc85c5d375dc566283
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2025-01-16 21:56:55 +0100

    new change

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,11 +2,10 @@ class BlogPostsController < ApplicationController
 before_action :set_blog_post, only: %i[show edit update destroy]
 before_action :authenticate_user!, except: %i[index show]
   def index
-    @blog_posts = BlogPost.all
+    @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
   end
 
   def show
-    
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
   end
@@ -50,11 +49,15 @@ before_action :authenticate_user!, except: %i[index show]
   private
 
   def blog_post_params
-    params.require(:blog_post).permit(:title, :body)
+    params.require(:blog_post).permit(:title, :body, :published_at)
   end
 
   def set_blog_post
-    @blog_post = BlogPost.find(params[:id])
+    if user_signed_in?
+      @blog_post = BlogPost.find(params[:id])
+    else
+      @blog_post = BlogPost.published.find(params[:id])
+    end
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
   end
