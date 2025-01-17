6886bf642963caad9077ee2b2e1c75a894b1af74
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2025-01-17 10:44:09 +0100

    add test file

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,40 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  test "draft? return true for draft blog post" do
+    assert blog_posts(:draft).draft?
+  end
+
+  test "draft? return false for draft blog post" do
+    refute blog_posts(:published).draft?
+  end
+
+  test "published? return true for published blog post" do
+    assert blog_posts(:published).published?
+  end
+
+  test "published? return false for draft blog post" do
+    refute blog_posts(:draft).published?
+  end
+
+  test "published? return false for scheduled blog post" do
+    refute blog_posts(:scheduled).published?
+  end
+
+  test "scheduled? return true for scheduled blog post" do
+    assert blog_posts(:scheduled).scheduled?
+  end
+
+  test "scheduled? return false for draft blog post" do
+    refute blog_posts(:draft).scheduled?
+  end
+
+  test "scheduled? return false for published blog post" do
+    refute blog_posts(:published).scheduled?
+  end
+
+
+  # def draft_blog_post
+  #   BlogPost.create(title: "Draft", published_at: nil)
   # end
 end
